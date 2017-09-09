7180a3c0eb7ea950696e2910155cc11c1a3c6b82
kemar1997 <kemar.1997@outlook.com>
2017-09-09 03:01:34 -0400

    initial commit

---
+++ models/index.js
@@ -0,0 +1,27 @@
+import Sequelize from "sequelize";
+// import User from './user';
+
+const sequelize = new Sequelize(
+	'test_graphql_db',
+	'test_graphql_admin',
+	'iamapassword',
+	{
+	host: 'localhost',
+	dialect: 'postgres',
+	},
+);
+
+const db = {
+	User: sequelize.import('./user'),
+}
+
+// Object.keys(db).forEach(function(modelName) {
+//  if ("associate" in db[modelName]) {
+//    db[modelName].associate(db);
+//  }
+// });
+
+db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+export default db;
